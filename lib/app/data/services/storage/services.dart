622c4a6377da7e342b6ed6decbe22a5bb032d734
mensur056 <mansur.sarxanov@gmail.com>
2022-03-12 09:51:12 +0100

    GetItDone

---
+++ lib/app/data/services/storage/services.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import 'package:get_it_done/app/core/utils/keys.dart';
+import 'package:get_storage/get_storage.dart';
+
+class StorageService extends GetxService {
+  late GetStorage _box;
+
+  Future<StorageService> init() async {
+    _box = GetStorage();
+    await _box.writeIfNull(taskKey, []);
+    return this;
+  }
+}
