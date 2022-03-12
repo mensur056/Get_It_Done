5dbe3b489f0b6035a9a939afbdbf6409666bcd36
mensur056 <mansur.sarxanov@gmail.com>
2022-03-12 09:33:41 +0100

    GetItDone

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get_it_done/app/modules/home/view.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const GetMaterialApp(debugShowCheckedModeBanner: false,
       title: "Get It Done",
       home: HomePage(),
     );
