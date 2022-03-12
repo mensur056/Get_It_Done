bd27951ce41b31d5141813b944a7525faef8e9fa
mensur056 <mansur.sarxanov@gmail.com>
2022-03-12 22:04:16 +0100

    GetItDone

---
+++ lib/app/data/provider/task/provider.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:get/get.dart';
+import 'package:get_it_done/app/core/utils/keys.dart';
+import 'package:get_it_done/app/data/services/storage/services.dart';
+
+import '../../models/task.dart';
+
+class TaskProvider {
+  StorageService _storage = Get.find<StorageService>();
+
+  // {'tasks':[{'color':'#ff123456','title':'Work','icon':0xe123}]}
+
+  List<Task> readTasks() {
+    var tasks = <Task>[];
+    jsonDecode(_storage.read(taskKey).toString())
+        .forEach((e) => tasks.add(Task.fromJson(e)));
+    return tasks;
+  }
+
+  void writeTasks(List<Task> tasks) {
+    _storage.write(taskKey, jsonEncode(tasks));
+  }
+}
