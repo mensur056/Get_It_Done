31f025c6a1522ea11bbd23ed4470c4d195b82331
mensur056 <mansur.sarxanov@gmail.com>
2022-03-12 10:15:48 +0100

    GetItDone

--- lib/app/data/models/task.dart
+++ lib/app/data/models/task.dart
@@ -10,4 +10,26 @@ class Task {
     required this.icon,
     this.todos,
   });
+
+  Task copyWith({
+    int? icon,
+    String? title,
+    String? color,
+    List<dynamic>? todos,
+  }) =>
+      Task(
+          title: title ?? this.title,
+          color: color ?? this.color,
+          todos: todos ?? this.todos,
+          icon: icon ?? this.icon);
+
+  factory Task.fromJson(Map<String, dynamic> json) => Task(
+        title: json['title'],
+        color: json['color'],
+        icon: json['icon'],
+        todos: json['todos'],
+      );
+
+  Map<String, dynamic> toJason() =>
+      {'title': title, 'icon': icon, 'color': color, 'todos': todos};
 }
