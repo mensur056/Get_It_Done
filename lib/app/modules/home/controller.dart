01aeb873b6ec0d5b24f72a008294982d5179f6a9
mensur056 <mansur.sarxanov@gmail.com>
2022-03-14 11:41:56 +0100

    GetItDone

--- lib/app/modules/home/controller.dart
+++ lib/app/modules/home/controller.dart
@@ -1,3 +1,8 @@
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get_it_done/app/data/services/storage/repository.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  TaskRepository taskRepository;
+
+  HomeController({required this.taskRepository});
+}
