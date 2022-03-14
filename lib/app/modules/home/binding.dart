5d1d63a9a6229ce3e8eb8af52c7f8c8818097a32
mensur056 <mansur.sarxanov@gmail.com>
2022-03-14 11:43:50 +0100

    GetItDone

--- lib/app/modules/home/binding.dart
+++ lib/app/modules/home/binding.dart
@@ -1,6 +1,12 @@
 import 'package:get/get.dart';
+import 'package:get_it_done/app/data/provider/task/provider.dart';
+import 'package:get_it_done/app/data/services/storage/repository.dart';
+import 'package:get_it_done/app/modules/home/controller.dart';
 
 class HomeBinding implements Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    Get.lazyPut(() => HomeController(
+        taskRepository: TaskRepository(taskProvider: TaskProvider())));
+  }
 }
