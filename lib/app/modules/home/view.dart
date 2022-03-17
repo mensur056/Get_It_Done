95e7c4cabf2e7e26edc194dc691534dc7c566ab1
mensur056 <mansur.sarxanov@gmail.com>
2022-03-17 08:57:45 +0100

    GetItDone

--- lib/app/modules/home/view.dart
+++ lib/app/modules/home/view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_it_done/app/core/utils/extensions.dart';
 import 'package:get_it_done/app/modules/home/controller.dart';
+import 'package:get_it_done/app/modules/home/widgets/add_card.dart';
 
 class HomePage extends GetView<HomeController> {
   const HomePage({Key? key}) : super(key: key);
@@ -13,12 +14,18 @@ class HomePage extends GetView<HomeController> {
         child: ListView(
           children: [
             Padding(
-              padding:  EdgeInsets.all(4.0.wp),
+              padding: EdgeInsets.all(4.0.wp),
               child: Text(
                 'My List',
-                style: TextStyle(fontSize: 24.0.sp, fontWeight: FontWeight.bold),
+                style:
+                    TextStyle(fontSize: 24.0.sp, fontWeight: FontWeight.bold),
               ),
             ),
+            GridView.count(
+              crossAxisCount: 2,
+              shrinkWrap: true,
+              physics: const ClampingScrollPhysics(),children: [AddCard()],
+            )
           ],
         ),
       ),
