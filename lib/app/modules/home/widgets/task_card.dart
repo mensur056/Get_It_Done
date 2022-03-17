16715ece9112752f21e7dac328f7e3bba38944c9
mensur056 <mansur.sarxanov@gmail.com>
2022-03-17 10:27:20 +0100

    GetItDone

--- lib/app/modules/home/widgets/task_card.dart
+++ lib/app/modules/home/widgets/task_card.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_it_done/app/core/utils/extensions.dart';
 import 'package:get_it_done/app/modules/home/controller.dart';
+import '../../../data/models/task.dart';
 
 class TaskCard extends StatelessWidget {
   final homeCtrl = Get.find<HomeController>();
+  final Task task;
 
-   TaskCard({Key? key}) : super(key: key);
+  TaskCard({Key? key, required this.task}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var squareWidth = Get.width - 12.0.wp;
     return Container();
   }
 }
