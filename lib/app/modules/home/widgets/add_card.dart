1bf9f8d8c2411b6ba28ae59fdd342354619e080c
mensur056 <mansur.sarxanov@gmail.com>
2022-03-17 08:35:46 +0100

    GetItDone

---
+++ lib/app/modules/home/widgets/add_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_it_done/app/modules/home/controller.dart';
+
+class AddCard extends StatelessWidget {
+  AddCard({Key? key}) : super(key: key);
+  final homeCtrl = Get.find<HomeController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
